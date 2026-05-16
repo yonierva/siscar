46ab41e99515c93ab36af02850c78935fff10869
yonierva <yoniervasquez98@gmail.com>
2026-05-16 08:44:00 -0500

    backend

---
+++ siscar-backend/src/main/java/com/siscar/siscar_backend/SiscarBackendApplication.java
@@ -0,0 +1,13 @@
+package com.siscar.siscar_backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SiscarBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SiscarBackendApplication.class, args);
+	}
+
+}
---
+++ siscar-backend/src/test/java/com/siscar/siscar_backend/SiscarBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.siscar.siscar_backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SiscarBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
